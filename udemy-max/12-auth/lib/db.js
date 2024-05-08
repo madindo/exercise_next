0110d6ac3524054ab27644050fa5d4cfd4c7e5e5
madindo <madindo@gmail.com>
2024-05-08 20:47:14 +0700

    remove cred

--- udemy-max/12-auth/lib/db.js
+++ udemy-max/12-auth/lib/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export async function connectToDatabase()  {
   const client = await MongoClient.connect(
-    "mongodb+srv://madindo:uAIKfovVbmWoh0EX@testing.lv2ecnz.mongodb.net/?retryWrites=true&w=majority&appName=Testing"
+    "mongodb+srv://test:******@testing.lv2ecnz.mongodb.net/?retryWrites=true&w=majority&appName=Testing"
   );
   return client;
 }
